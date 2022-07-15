f6fef975d5fe95b3531f86f62897cf4f264a7e4f
agu <iba.agustin@gmail.com>
2022-07-15 19:41:34 -0300

    Removing CRA boilerplate, setting up firebase skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebase from "firebase/compat/app";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase-config";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+console.log(database);
+
+// adding ghost users
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuGsC2S3OQKeL5z-v1s4-DQWDYtvG8mnA",
+
+  authDomain: "onesy-9459d.firebaseapp.com",
+
+  projectId: "onesy-9459d",
+
+  storageBucket: "onesy-9459d.appspot.com",
+
+  messagingSenderId: "231351094584",
+
+  appId: "1:231351094584:web:d8d936ad5ed490a494c5cd",
+};
+
+export { firebaseConfig };
